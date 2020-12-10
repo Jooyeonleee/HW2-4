652c937b83966d1dfed12facec9198c864bfa8b0
ps0640 <43405705+ps0640@users.noreply.github.com>
2020-12-11 02:40:58 +0900

    1715295

--- MainProgram.java
+++ MainProgram.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class git1test {
+public class MainProgram {
     public static void main(String[] args) {
         MainProgram mainProgram = new MainProgram();
         mainProgram.printBanner();
